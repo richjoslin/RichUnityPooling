213c5284ecb12376cee12ee179dd6256f1a0ed86
Rich Joslin <richjoslin@richjoslin.com>
2016-12-27 16:51:01 -0800

    initial import of project and bundle

---
+++ Unity Project/Assets/RichJoslin/Pooling/Examples/Scripts/ExampleGUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ExampleGUI : MonoBehaviour
+{
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+		{
+			GUILayout.BeginHorizontal();
+			{
+				GUILayout.FlexibleSpace();
+				GUILayout.BeginVertical();
+				{
+					GUILayout.Space(100f);
+					if (GUILayout.Button("Generate Ball"))
+					{
+						PoolBall poolBall = PoolBall.Generate();
+					}
+					GUILayout.FlexibleSpace();
+				}
+				GUILayout.EndVertical();
+				GUILayout.FlexibleSpace();
+			}
+			GUILayout.EndHorizontal();
+		}
+		GUILayout.EndArea();
+	}
+}
---
+++ Unity Project/Assets/RichJoslin/Pooling/Examples/Scripts/PoolBall.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using RichJoslin.Pooling;
+
+public class PoolBall : MonoBehaviour, IPooledInstanceGet, IPooledInstanceReturn
+{
+	public static PoolBall Generate()
+	{
+		PoolBall poolBall = PoolManager.I.GetPooler("Ball Key").Get<PoolBall>();
+		poolBall.StartCoroutine("LifetimeLoop");
+		return poolBall;
+	}
+
+	public IEnumerator LifetimeLoop()
+	{
+		yield return new WaitForSeconds(2f);
+		PoolManager.I.Return(this);
+	}
+
+	// IPooledInstanceGet
+	public void OnGet(UnityEngine.Object arg = null)
+	{
+		// do stuff as soon as it's gotten from a pooler
+	}
+
+	// IPooledInstanceReturn
+	public void OnReturn()
+	{
+		// do stuff as soon as it's returned to a pooler
+	}
+}
